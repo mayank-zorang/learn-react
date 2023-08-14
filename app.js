c8d92294f818247c840f8cc7de53308bfca9d55a
Mayank <mayank.paswan@zorang.com>
2023-08-14 16:20:23 +0530

    Ep 04 working/props/state/destructuring/chaining/mapfunciton

--- app.js
+++ app.js
@@ -1,39 +1,105 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import  ReactDOM  from "react-dom/client";  
+import restaurantList from "./restaurantList";
 
-//React Element
-const Heading = (
-    <h1 className="heading">
-        Welecome to JSX Syntax😂
-    </h1>  
-);
+/*
+ Header 
+    -logo
+    -Nav Items
+ Body
+    -Search
+    -RestaurantContainer
+    -RestaurantCard
+        - Food-Image
+        - Name, Rating, Price, cuisine  delivarytime etc
+Footer
+    -copyright
+    -Links
+    -Addressh    
+*/
 
-const elem = <span>React Element </span>
+//Header Component
 
-//Normal function components
-const Title = function (){
+const Header = () =>{
     return (
-        <h1 className="head">
-            {elem}
-             Title 😂
-        </h1>  
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://seeklogo.com/images/B/burger-food-logo-5B8B17C290-seeklogo.com.png"/>
+            </div>
+            <div className="nav-items"> 
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+
+//RestaurantCard Component
+const RestaurantCard = (props) => {
+    // console.log(props)
+    const {resData} = props;
+    const {
+        name,
+        cuisines,
+        avgRating,
+        costForTwo,
+        deliveryTime,
+        cloudinaryImageId
+    } = resData?.data;
+    return(
+        <div className="res-card">
+            <img 
+                className="res-logo"
+                alt="res-logo"
+                src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} />
+            <h3>{name}</h3>
+            <h5>{cuisines.join(", ")}</h5>
+            <h4>{avgRating}⭐</h4>
+            <h4>₹{costForTwo / 40}</h4>
+            <h4>{deliveryTime} minutes</h4>
+        </div>
     );
-} 
+}
 
-//React Components | arrow function
-// it's component composition 
 
-const data = 1000;
-const FunctionComponent = () => (
-    <div className="container">
-     <Title />
-     <h2>localhost:{data + 234}</h2>
-     {Title()}
-     <h1 className="heading">I am Function Component </h1> 
-    </div>
-)
+//Restaurant Data
+
+
+//body Component
+
+//not using keys(not acceptable) <<<< index as key <<<<<<<<< unique id (best practice)
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {
+                    restaurantList.map((res)=>{
+                        return <RestaurantCard  key={res.data.id} resData ={res}/>
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 // root.render(Heading) 
-root.render(<FunctionComponent />)   
+root.render(<AppLayout />) 
